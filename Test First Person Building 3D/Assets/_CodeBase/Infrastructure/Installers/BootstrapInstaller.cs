0629e28a7ae772823cac972baea13873230591c9
Michael Dvin <idewishortcut@gmail.com>
2024-08-24 18:37:05 +0500

    Add Installers

---
+++ Test First Person Building 3D/Assets/_CodeBase/Infrastructure/Installers/BootstrapInstaller.cs
@@ -0,0 +1,15 @@
+﻿using Zenject;
+
+namespace _CodeBase.Infrastructure.Installers
+{
+    public class BootstrapInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+
+        private void BindBootstrapper()
+        {
+        }
+    }
+}
